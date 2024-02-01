59338add12e5e0b18a9fb16c90a76793c3edd61f
lvanjelg <lukevanjelg@gmail.com>
2024-02-01 12:23:56 -0500

    .

--- src/main/java/edu/iu/c322/test1/Test1Application.java
+++ src/main/java/edu/iu/c322/test1/Test1Application.java
@@ -1,11 +1,14 @@
 package edu.iu.c322.test1;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
+@SpringBootConfiguration
 public class Test1Application {
-
+	@GetMapping("/")
 	public static void main(String[] args) {
 		SpringApplication.run(Test1Application.class, args);
 	}
